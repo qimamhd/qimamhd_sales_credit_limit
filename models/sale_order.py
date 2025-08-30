5e382c0c54c76bb226c65bfb36fb4ba3471cfe7b
qimamhd <106440499+qimamhd@users.noreply.github.com>
2025-08-30 13:23:00 +0300

    update

--- models/sale_order.py
+++ models/sale_order.py
@@ -31,7 +31,7 @@ class xx_account_move(models.Model):
                     current_balance = rec.validation_customer_balance()
 
                     # new_balance = balance + rec.amount_total
-                    if current_balance > rec.partner_id.credit_limit:
+                    if current_balance + rec.amount_total > rec.partner_id.credit_limit:
                             raise ValidationError(
                                         "تنبيه .. رصيد العميل [%s] تجاوز الحد الائتماني  .. الحد الائتماني [ %s ]>>> رصيد العميل [ %s ]" % (
                                             rec.partner_id.name, rec.partner_id.credit_limit, current_balance))
