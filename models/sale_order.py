c7b0b0371345c7ec6302342407b07a0360a1ff95
qimamhd <106440499+qimamhd@users.noreply.github.com>
2025-08-30 13:09:52 +0300

    update

--- models/sale_order.py
+++ models/sale_order.py
@@ -21,7 +21,7 @@ class xx_account_move(models.Model):
            
             return x
 
-    def actionc_confirm(self):
+    def action_confirm(self):
         for rec in self:
             current_balance = rec.validation_customer_balance()
 
@@ -31,7 +31,7 @@ class xx_account_move(models.Model):
                                 "تنبيه .. رصيد العميل [%s] تجاوز الحد الائتماني  .. الحد الائتماني [ %s ]>>> رصيد العميل [ %s ]" % (
                                     rec.partner_id.name, rec.partner_id.credit_limit, current_balance))
 
-        invoice = super(xx_account_move, self).actionc_confirm()
+        invoice = super(xx_account_move, self).action_confirm()
         
         return invoice
      
